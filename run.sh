d656c2f3445abd2fc04f81b1e5d879c39f6eaf26
Mark Brooks <mark@onefoursix.com>
2014-08-07 22:30:29 -0700

    updated for CDH 5.1

--- run.sh
+++ run.sh
@@ -1,20 +1,22 @@
 HADOOP_CLIENT_DIR=/opt/cloudera/parcels/CDH/lib/hadoop/client/
 HIVE_LIB_DIR=/opt/cloudera/parcels/CDH/lib/hive/lib
-IMPALA_LIB_DIR=/opt/cloudera/parcels/IMPALA/lib/impala/lib
+IMPALA_LIB_DIR=/opt/cloudera/parcels/CDH/lib/impala/lib
 
-CLASSPATH=$HIVE_LIB_DIR/hive-jdbc-0.10.0-cdh4.7.0.jar
-CLASSPATH=$CLASSPATH:$HIVE_LIB_DIR/hive-metastore-0.10.0-cdh4.7.0.jar
-CLASSPATH=$CLASSPATH:$HIVE_LIB_DIR/hive-common-0.10.0-cdh4.7.0.jar
-CLASSPATH=$CLASSPATH:$HIVE_LIB_DIR/hive-service-0.10.0-cdh4.7.0.jar
+CLASSPATH=$HIVE_LIB_DIR/hive-jdbc.jar
+CLASSPATH=$CLASSPATH:$HIVE_LIB_DIR/hive-metastore.jar
+CLASSPATH=$CLASSPATH:$HIVE_LIB_DIR/hive-common.jar
+CLASSPATH=$CLASSPATH:$HIVE_LIB_DIR/hive-service.jar
 CLASSPATH=$CLASSPATH:$HIVE_LIB_DIR/libfb303-0.9.0.jar
-CLASSPATH=$CLASSPATH:$HIVE_LIB_DIR/libthrift-0.9.0-cdh4-1.jar
-CLASSPATH=$CLASSPATH:$HIVE_LIB_DIR/commons-logging-1.0.4.jar
+CLASSPATH=$CLASSPATH:$HIVE_LIB_DIR/libthrift-0.9.0.cloudera.2.jar
+CLASSPATH=$CLASSPATH:$HIVE_LIB_DIR/commons-logging-1.1.3.jar
 CLASSPATH=$CLASSPATH:$HIVE_LIB_DIR/log4j-1.2.16.jar
-CLASSPATH=$CLASSPATH:$HIVE_LIB_DIR/slf4j-api-1.6.4.jar
+CLASSPATH=$CLASSPATH:$HIVE_LIB_DIR/slf4j-api-1.7.5.jar
 
-CLASSPATH=$CLASSPATH:$IMPALA_LIB_DIR/slf4j-log4j12-1.6.1.jar
+CLASSPATH=$CLASSPATH:$IMPALA_LIB_DIR/slf4j-log4j12-1.7.5.jar
 
 CLASSPATH=$CLASSPATH:$HADOOP_CLIENT_DIR/hadoop-common.jar
+CLASSPATH=$CLASSPATH:$HADOOP_CLIENT_DIR/httpcore.jar
+CLASSPATH=$CLASSPATH:$HADOOP_CLIENT_DIR/httpclient.jar
 
 CLASSPATH=$CLASSPATH:./cloudera-impala-jdbc-example-1.0.jar
 
